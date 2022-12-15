8d51a165151110c84bbc23387570f6c063c8a490
Marcus <marcus.schweizer91@gmail.com>
2022-12-15 17:14:26 +0100

    Add CardController

---
+++ backend/src/main/java/com/example/backend/controller/CardController.java
@@ -0,0 +1,27 @@
+package com.example.backend.controller;
+
+import com.example.backend.models.Card;
+import com.example.backend.service.CardService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cards")
+public class CardController {
+
+    private final CardService cardService;
+
+    public CardController(CardService cardService) {
+        this.cardService = cardService;
+    }
+
+    @GetMapping
+    public List<Card> getAllMovies(){
+        return cardService.getAllCards();
+    }
+
+
+}
