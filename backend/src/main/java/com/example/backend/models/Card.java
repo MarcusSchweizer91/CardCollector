21972e21815509ac793bc7973a15e1c89607a9ff
Marcus <marcus.schweizer91@gmail.com>
2022-12-15 16:28:39 +0100

    Add Model Card & Attacks

---
+++ backend/src/main/java/com/example/backend/models/Card.java
@@ -0,0 +1,15 @@
+package com.example.backend.models;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document("cards")
+
+public record Card(
+        String id,
+        String name,
+        String hp,
+        List<Attacks> attacks
+) {
+}
