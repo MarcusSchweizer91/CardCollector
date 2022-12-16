3062a02684948886cf2b995e782996d957891f4f
Marcus <marcus.schweizer91@gmail.com>
2022-12-16 19:03:46 +0100

    BugFix usecards

--- frontend/src/CardGallery/components/hooks/useCards.tsx
+++ frontend/src/CardGallery/components/hooks/useCards.tsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import axios from "axios/index";
+import axios from "axios";
 import {Card} from "../../models/Card";
 
 export default function useCards(){
