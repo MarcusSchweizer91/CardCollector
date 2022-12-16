d3deefa855521944c94521402557bb3c00c2dce7
Marcus <marcus.schweizer91@gmail.com>
2022-12-16 10:57:59 +0100

    Add CardControllerIntegrationTest

---
+++ backend/src/test/java/com/example/backend/controller/CardControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.backend.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CardControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+
+    @Test
+    void getAllMovies_whenListEmpty_ThenReturn() throws Exception {
+
+        mockMvc.perform(get("/api/cards"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+}
