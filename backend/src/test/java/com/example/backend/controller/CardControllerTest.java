17ccbe1d391300d37c21748fba883f54301646bf
Marcus <marcus.schweizer91@gmail.com>
2022-12-16 17:43:08 +0100

    Add getCardByIDIntegrationTest

--- backend/src/test/java/com/example/backend/controller/CardControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/CardControllerTest.java
@@ -1,11 +1,16 @@
 package com.example.backend.controller;
 
 
+import com.example.backend.models.Card;
+import com.example.backend.repo.CardRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
@@ -21,13 +26,37 @@ class CardControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private CardRepo cardRepo;
+
 
 
     @Test
+    @DirtiesContext
     void getAllCard_whenListEmpty_ThenReturn() throws Exception {
 
         mockMvc.perform(get("/api/cards"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("[]"));
+    }
+
+    @Test
+    @DirtiesContext
+    void getCardById() throws Exception {
+
+        Card card = new Card("1", "Pikachu", "120", Collections.emptyList());
+        Card result = cardRepo.save(card);
+
+        mockMvc.perform(get("/api/cards/" + result.id()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                        "id": "<ID>",
+                        "name": "Pikachu",
+                        "hp": "120",
+                        "attacks":[]
+                        }
+                        
+                        """.replace("<ID>", result.id())));
     }
 }
