af879ed3501aa79a9381d85198fb5e6a333b6ff9
Marcus <marcus.schweizer91@gmail.com>
2022-12-18 22:37:54 +0100

    Add imports

--- frontend/src/CardGallery/components/PokemonApp.tsx
+++ frontend/src/CardGallery/components/PokemonApp.tsx
@@ -2,33 +2,41 @@ import {useState} from "react";
 import PokemonGallery from "./PokemonGallery";
 import SearchBar from "./SearchBar";
 import useCards from "./hooks/useCards";
+import NavBar from "./NavBar";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 export default function PokemonApp() {
 
-   const {pokeCards} = useCards()
+    const {pokeCards} = useCards()
 
     const [searchText, setSearchText] = useState<string>("")
 
 
-    function handleSearchOnChange (searchText: string){
+    function handleSearchOnChange(searchText: string) {
         setSearchText(searchText)
     }
 
-    const filteredPokeCards = pokeCards.filter((pokeCard)=>
+    const filteredPokeCards = pokeCards.filter((pokeCard) =>
         pokeCard.id.toLowerCase().includes(searchText.toLowerCase()) ||
         pokeCard.name.toLowerCase().includes(searchText.toLowerCase())
     )
 
 
-
-
-
     return (
         <div>
             <div>
-                <SearchBar handleSearchText={handleSearchOnChange}/>
+                <BrowserRouter>
+                    <NavBar/>
+                    <SearchBar handleSearchText={handleSearchOnChange}/>
+                    <Routes>
+                        <Route path={"/"} element={<PokemonGallery cards={filteredPokeCards}/>}></Route>
+
+                    </Routes>
+                </BrowserRouter>
+
+
             </div>
-            <PokemonGallery cards={filteredPokeCards}/>
+
         </div>
     )
 }
