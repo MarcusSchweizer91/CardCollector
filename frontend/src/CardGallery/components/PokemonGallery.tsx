537aea2cbdddd0072d16a0631db29a9db1220bf2
Marcus <marcus.schweizer91@gmail.com>
2022-12-18 19:23:19 +0100

    Add Container to Gallery

--- frontend/src/CardGallery/components/PokemonGallery.tsx
+++ frontend/src/CardGallery/components/PokemonGallery.tsx
@@ -1,22 +1,27 @@
 import {PokeCard} from "../models/PokeCard";
 import PokemonCard from "./PokemonCard";
-import {Grid} from "@mui/material";
+import {Container, Grid, Typography} from "@mui/material";
 
-type PokemonGalleryProps={
+type PokemonGalleryProps = {
     cards: PokeCard[]
 }
 
-export default function PokemonGallery(props:PokemonGalleryProps){
-    
-    const cardComponents = props.cards.map(cardData =>{
+export default function PokemonGallery(props: PokemonGalleryProps) {
+
+    const cardComponents = props.cards.map(cardData => {
         return <PokemonCard card={cardData} key={cardData.id}/>
     })
-    
-    return(
+
+    return (
         <div>
-            <Grid container justifyContent={"space-evenly"} >
-               {cardComponents}
-            </Grid>
+            <Container>
+                <Typography variant={"h2"} align={"center"} m={2}>
+                    All Cards
+                </Typography>
+                <Grid container justifyContent={"space-evenly"}>
+                    {cardComponents}
+                </Grid>
+            </Container>
         </div>
     )
 }
