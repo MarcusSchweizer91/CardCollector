2bbc758dd73fcc909ea43fb64ddbe4f58bb8ffe8
Marcus <marcus.schweizer91@gmail.com>
2022-12-18 18:12:11 +0100

    Adding Card Area with Mui

--- frontend/src/CardGallery/components/PokemonCard.tsx
+++ frontend/src/CardGallery/components/PokemonCard.tsx
@@ -1,26 +1,56 @@
-import {Card} from "../models/Card";
-
+import {Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography} from "@mui/material";
+import {PokeCard} from "../models/PokeCard";
 
 
 type PokemonCardProps = {
-    card: Card
+    card: PokeCard
 
 }
 
 export default function PokemonCard(props: PokemonCardProps) {
 
 
-
-    const attacksComponents = props.card.attacks.map((attacks)=>{
+    const attacksComponents = props.card.attacks.map((attacks) => {
         return attacks.name;
     })
 
-    return (
-        <div>
-            <h2>{props.card.name}</h2>
-            <p>{props.card.hp}</p>
-            <p>{attacksComponents}</p>
-        </div>
-    )
 
+    return (
+
+        <Grid m={6}>
+            <Card>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        image={props.card.images.small}
+                        alt="Pokeball"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {props.card.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {props.card.hp}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {attacksComponents}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Favorites
+                    </Button>
+                    <Button size="small" color="primary">
+                        Buy
+                    </Button>
+                    <Button size="small" color="primary">
+                        Trade
+                    </Button>
+                </CardActions>
+            </Card>
+        </Grid>
+
+
+    )
 }
