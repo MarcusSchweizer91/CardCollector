7a6e6b4043e9b7b367c5345694c427e4c1ab154a
Marcus <marcus.schweizer91@gmail.com>
2022-12-20 11:21:39 +0100

    Add ExchangeMovieDTO & IDService to saveEntry

--- backend/src/main/java/com/example/backend/service/ExchangeService.java
+++ backend/src/main/java/com/example/backend/service/ExchangeService.java
@@ -1,6 +1,7 @@
 package com.example.backend.service;
 
 import com.example.backend.models.ExchangeCard;
+import com.example.backend.models.ExchangeCardDTO;
 import com.example.backend.repo.ExchangeRepo;
 import org.springframework.stereotype.Service;
 
@@ -9,16 +10,19 @@ public class ExchangeService {
 
     private final ExchangeRepo exchangeRepo;
 
-    public ExchangeService(ExchangeRepo exchangeRepo) {
+    private final IDService idService;
+
+    public ExchangeService(ExchangeRepo exchangeRepo, IDService idService) {
         this.exchangeRepo = exchangeRepo;
+        this.idService = idService;
     }
 
 
 
-    public ExchangeCard saveEntry(ExchangeCard exchangeCard){
+    public ExchangeCard saveEntry(ExchangeCardDTO exchangeCard){
 
         ExchangeCard newCartToExchange = new ExchangeCard(
-                exchangeCard.id(),
+                idService.generateID(),
                 exchangeCard.name(),
                 exchangeCard.description(),
                 exchangeCard.type(),
