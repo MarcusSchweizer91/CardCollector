19e525795662b840a67566d3bc8679d341a4760c
Marcus <marcus.schweizer91@gmail.com>
2022-12-20 09:37:58 +0100

    Add ExchangeController with POST

---
+++ backend/src/main/java/com/example/backend/controller/ExchangeController.java
@@ -0,0 +1,27 @@
+package com.example.backend.controller;
+
+
+import com.example.backend.models.ExchangeCard;
+import com.example.backend.service.ExchangeService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/exchange")
+
+public class ExchangeController {
+
+    private final ExchangeService exchangeService;
+
+    public ExchangeController(ExchangeService exchangeService) {
+        this.exchangeService = exchangeService;
+    }
+
+    @PostMapping
+    public ExchangeCard addEntry (@RequestBody ExchangeCard entry){
+        return exchangeService.saveEntry(entry);
+    }
+
+}
