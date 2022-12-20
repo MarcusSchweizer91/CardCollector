f998936f93243053c38e02cdccdba7174411e7d2
Marcus <marcus.schweizer91@gmail.com>
2022-12-20 09:26:25 +0100

    Add model ExchangeCard

---
+++ backend/src/main/java/com/example/backend/models/ExchangeCard.java
@@ -0,0 +1,14 @@
+package com.example.backend.models;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("exchangeOffers")
+public record ExchangeCard(
+String id,
+String name,
+String description,
+String type,
+String price,
+String alternative
+) {
+}
