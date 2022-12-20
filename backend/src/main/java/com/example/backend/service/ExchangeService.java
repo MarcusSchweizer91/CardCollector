4be17038b9c54b29dc4a7741679644d2bca888ae
Marcus <marcus.schweizer91@gmail.com>
2022-12-20 13:23:45 +0100

    Add getAllEntries

--- backend/src/main/java/com/example/backend/service/ExchangeService.java
+++ backend/src/main/java/com/example/backend/service/ExchangeService.java
@@ -5,12 +5,18 @@ import com.example.backend.models.ExchangeCardDTO;
 import com.example.backend.repo.ExchangeRepo;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ExchangeService {
 
     private final ExchangeRepo exchangeRepo;
 
     private final IDService idService;
+
+    public List<ExchangeCard> getAllEntries(){
+        return exchangeRepo.findAll();
+    }
 
     public ExchangeService(ExchangeRepo exchangeRepo, IDService idService) {
         this.exchangeRepo = exchangeRepo;
