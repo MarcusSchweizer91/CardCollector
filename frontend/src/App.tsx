dc3cc767c55c34127acbd7d663b212250de79aa2
Marcus <marcus.schweizer91@gmail.com>
2022-12-20 13:54:27 +0100

    Transfer BrowserRouter to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
 import PokemonApp from "./CardGallery/components/PokemonCards/PokemonApp";
+import NavBar from "./CardGallery/components/NavBar";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import ExchangeApp from "./CardGallery/components/CardExchange/ExchangeApp";
+import PokemonDetails from "./CardGallery/components/PokemonCards/PokemonDetails";
 
 function App() {
 
 
   return (
     <div className="App">
-      <PokemonApp/>
+        <BrowserRouter>
+            <NavBar/>
+            <Routes>
+                <Route path={"/"} element={<PokemonApp/>}></Route>
+                <Route path={"/exchange"} element={<ExchangeApp/>}></Route>
+                <Route path={"/user"} element={<PokemonApp/>}></Route>
+                <Route path={"/details/:id"} element={<PokemonDetails/>}></Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
