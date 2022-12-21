e25e32454caaacdf9940c348e203ad8116fc03d9
Marcus <marcus.schweizer91@gmail.com>
2022-12-21 16:13:29 +0100

    Add mui Card & Button

--- frontend/src/CardGallery/components/CardExchange/ExchangeCard.tsx
+++ frontend/src/CardGallery/components/CardExchange/ExchangeCard.tsx
@@ -1,29 +1,32 @@
 import {CardToExchange} from "../../models/CardToExchange";
-import {Button} from "@mui/material";
+import {Button, Card} from "@mui/material";
 
-type ExchangeCardProps ={
+
+type ExchangeCardProps = {
     exchangeCard: CardToExchange
-    deleteEntry:(id?:string)=>void
+    deleteEntry: (id?: string) => void
 }
 
 
-export default function ExchangeCard(props: ExchangeCardProps){
+export default function ExchangeCard(props: ExchangeCardProps) {
 
-function onClickDelete (){
-    props.deleteEntry(props.exchangeCard.id)
+    function onClickDelete() {
+        props.deleteEntry(props.exchangeCard.id)
 
-}
+    }
 
-    return(
+    return (
         <div>
-            <h5>{props.exchangeCard.name}</h5>
-            <p>{props.exchangeCard.description}</p>
-            <p>{props.exchangeCard.type}</p>
-            <p>{props.exchangeCard.price}</p>
-            <p>{props.exchangeCard.alternative}</p>
+            <Card variant={"outlined"} sx={{m:'1rem', maxWidth:'30rem'}}>
+                <h5>{props.exchangeCard.name}</h5>
+                <p>{props.exchangeCard.description}</p>
+                <p>{props.exchangeCard.type}</p>
+                <p>{props.exchangeCard.price}</p>
+                <p>{props.exchangeCard.alternative}</p>
 
-            <Button onClick={onClickDelete}>Delete</Button>
-            <Button>Edit</Button>
+                <Button sx={{mb:'1rem', mr:'.5rem'}} variant={'outlined'} onClick={onClickDelete}>Delete</Button>
+                <Button sx={{mb:'1rem', ml:'.5rem'}} variant={'outlined'}>Edit</Button>
+            </Card>
         </div>
     )
 }
