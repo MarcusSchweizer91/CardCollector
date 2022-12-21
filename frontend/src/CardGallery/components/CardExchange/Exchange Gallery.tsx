73e5b240d8519a7f2245832c997cf34a797a314f
Marcus <marcus.schweizer91@gmail.com>
2022-12-21 11:19:31 +0100

    Add deleteEntry

--- frontend/src/CardGallery/components/CardExchange/Exchange Gallery.tsx
+++ frontend/src/CardGallery/components/CardExchange/Exchange Gallery.tsx
@@ -3,12 +3,13 @@ import ExchangeCard from "./ExchangeCard";
 
 type ExchangeGalleryProps ={
     entries: CardToExchange[]
+    deleteEntry:(id?:string)=>void
 }
 
 export default function ExchangeGallery(props: ExchangeGalleryProps){
 
     const exchangeComponents = props.entries.map(exchangeData =>{
-        return <ExchangeCard exchangeCard={exchangeData} key={exchangeData.id}/>
+        return <ExchangeCard exchangeCard={exchangeData} key={exchangeData.id} deleteEntry={props.deleteEntry}/>
     })
 
     return(
