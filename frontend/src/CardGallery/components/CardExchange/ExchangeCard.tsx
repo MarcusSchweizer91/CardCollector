b3cf8b108794ac55a9dcecfd37092606f5f2b197
Marcus <marcus.schweizer91@gmail.com>
2022-12-23 19:30:50 +0100

    Add edit Entry

--- frontend/src/CardGallery/components/CardExchange/ExchangeCard.tsx
+++ frontend/src/CardGallery/components/CardExchange/ExchangeCard.tsx
@@ -1,60 +1,115 @@
 import {CardToExchange} from "../../models/CardToExchange";
-import {Button, Card, CardActionArea, CardActions, CardContent, Grid, Typography} from "@mui/material";
+import {Button, Card, CardActions, CardContent, Grid, Typography} from "@mui/material";
 import {useNavigate} from "react-router-dom";
+import {ChangeEvent, useState} from "react";
+import "../css/ExchangeCard.css"
 
 
 type ExchangeCardProps = {
     exchangeCard: CardToExchange
     deleteEntry: (id?: string) => void
+    editEntry: (newEdit: CardToExchange) => void
 }
 
 
 export default function ExchangeCard(props: ExchangeCardProps) {
 
-
+    const [showButton, setShowButton] = useState<boolean>(false)
     const navigate = useNavigate()
+
+    const [name, setName] = useState(props.exchangeCard.name)
+    const [description, setDescription] = useState(props.exchangeCard.description)
+    const [type, setType] = useState(props.exchangeCard.type)
+    const [price, setPrice] = useState(props.exchangeCard.price)
+    const [alternative, setAlternative] = useState(props.exchangeCard.alternative)
+
+    function onClickEditButton() {
+        setShowButton(true)
+        console.log(showButton)
+    }
+
+    function handleChange() {
+        const newEdit: CardToExchange = {
+            id: props.exchangeCard.id,
+            name: name,
+            description: description,
+            type: type,
+            price: price,
+            alternative: alternative
+        }
+        props.editEntry(newEdit)
+        setShowButton(false)
+    }
+
 
     function onClickDelete() {
         props.deleteEntry(props.exchangeCard.id)
 
     }
 
-    function handleDetailsClick(){
+    function handleDetailsClick() {
         navigate("/exchange/" + props.exchangeCard.id)
     }
+
     return (
         <div>
             <Grid m={6}>
                 <Card>
-                    <CardActionArea onClick={handleDetailsClick}>
 
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {props.exchangeCard.name}
-                            </Typography>
-                            <Typography gutterBottom variant="body1" component="div">
-                                {props.exchangeCard.description}
-                            </Typography>
-                            <Typography gutterBottom variant="body1" component="div">
-                                {props.exchangeCard.type}
-                            </Typography>
-                            <Typography gutterBottom variant="body1" component="div">
-                                {props.exchangeCard.price}
-                            </Typography>
-                            <Typography gutterBottom variant="body1" component="div">
-                                {props.exchangeCard.alternative}
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
+
+                    <CardContent>
+                        <Typography className={showButton ? "disappear" : "show"} gutterBottom variant="h5"
+                                    component="div">
+                            {props.exchangeCard.name}
+                        </Typography>
+                        <input className={showButton ? "show" : "disappear"} type={"text"}
+                               onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                   setName(event.target.value)
+                               }} value={name}/>
+
+                        <Typography gutterBottom variant="body1" component="div">
+                            {props.exchangeCard.description}
+                        </Typography>
+                        <input className={showButton ? "show" : "disappear"} type={"text"}
+                               onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                   setDescription(event.target.value)
+                               }} value={description}/>
+
+                        <Typography gutterBottom variant="body1" component="div">
+                            {props.exchangeCard.type}
+                        </Typography>
+                        <input className={showButton ? "show" : "disappear"} type={"text"}
+                               onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                   setType(event.target.value)
+                               }} value={type}/>
+                        <Typography gutterBottom variant="body1" component="div">
+                            {props.exchangeCard.price}
+                        </Typography>
+                        <input className={showButton ? "show" : "disappear"} type={"text"}
+                               onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                   setPrice(event.target.value)
+                               }} value={price}/>
+                        <Typography gutterBottom variant="body1" component="div">
+                            {props.exchangeCard.alternative}
+                            <input className={showButton ? "show" : "disappear"} type={"text"}
+                                   onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                       setAlternative(event.target.value)
+                                   }} value={alternative}/>
+                        </Typography>
+                    </CardContent>
+
+
+
                     <CardActions>
                         <Button variant={"outlined"} onClick={onClickDelete} size="small" color="primary">
                             Delete
                         </Button>
-                        <Button variant={"outlined"} size="small" color="primary">
-                            Buy
+                        <Button onClick={handleDetailsClick} variant={"outlined"} size="small" color="primary">
+                            Details
                         </Button>
-                        <Button variant={"outlined"} size="small" color="primary">
-                            Trade
+                        <Button onClick={onClickEditButton} variant={"outlined"} size="small" color="primary" className={showButton? "disappear" : "show"} > Edit </Button>
+                        <Button onClick={handleChange} variant={"outlined"} size="small" color="primary" className={showButton? "show" : "disappear"}>
+                            Save
                         </Button>
                     </CardActions>
                 </Card>
