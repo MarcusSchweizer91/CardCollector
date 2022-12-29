df2108cbd16894ff887a67c02263f992d0781386
Marcus <marcus.schweizer91@gmail.com>
2022-12-29 15:10:43 +0100

    Create ExchangeServiceTests & IntegrationTests

--- backend/src/test/java/com/example/backend/controller/ExchangeControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/ExchangeControllerTest.java
@@ -2,6 +2,7 @@ package com.example.backend.controller;
 
 import com.example.backend.models.ExchangeCard;
 
+import com.example.backend.repo.ExchangeRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,8 +14,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,13 +26,16 @@ class ExchangeControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private ExchangeRepo exchangeRepo;
+
 
     @Test
     void getAllEntries_whenListEmpty_thenReturn() throws Exception {
 
-            mockMvc.perform(get("/api/exchange"))
-                    .andExpect(status().isOk())
-                    .andExpect(content().json("[]"));
+        mockMvc.perform(get("/api/exchange"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
 
 
     }
@@ -46,9 +49,9 @@ class ExchangeControllerTest {
                         .content(
                                 """
                                         {
-                                        "name": "Pikachu",                                     
+                                        "name": "Pikachu",
                                         "description": "ABC",
-                                        "type": "search",                                      
+                                        "type": "search",
                                         "price": 8.50,
                                         "alternative": "Trade for two Glurak"
                                         }
@@ -61,4 +64,95 @@ class ExchangeControllerTest {
         ExchangeCard cardToExchange = objectMapper.readValue(content, ExchangeCard.class);
         assertNotNull(cardToExchange.id());
     }
+
+    @Test
+    @DirtiesContext
+    void getEntryByID() throws Exception {
+
+        ExchangeCard cardToExchange = new ExchangeCard(
+                "1",
+                "Pikachu",
+                "BlaBla",
+                "search",
+                "12",
+                "Two Glurak"
+
+        );
+        ExchangeCard result = exchangeRepo.save(cardToExchange);
+        mockMvc.perform(get("/api/exchange/" + result.id()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                           "id": "<ID>",
+                           "name": "Pikachu",
+                           "description": "BlaBla",
+                           "type": "search",
+                           "price": "12",
+                            "alternative": "Two Glurak"
+                        }
+                        """.replace("<ID>", result.id())));
+    }
+
+    @Test
+    @DirtiesContext
+    void updateEntry() throws Exception {
+        ExchangeCard cardToExchange = new ExchangeCard(
+                "1",
+                "Pikachu",
+                "BlaBla",
+                "search",
+                "12",
+                "Two Glurak"
+
+        );
+        exchangeRepo.save(cardToExchange);
+
+        mockMvc.perform(put("/api/exchange/" + cardToExchange.id())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                """
+                                             {
+                                         "id": "1",
+                                        "name": "Bisasam",
+                                        "description": "BlaBla",
+                                        "type": "search",
+                                        "price": "12",
+                                        "alternative": "Two Glurak"
+                                             }
+                                                     """
+                        ))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        """
+                                      {
+                                "id": "1",
+                                "name": "Bisasam",
+                                "description": "BlaBla",
+                                "type": "search",
+                                "price": "12",
+                                 "alternative": "Two Glurak"
+                                      }
+                                      """
+                ));
+    }
+
+    @Test
+    @DirtiesContext
+    void deleteEntry() throws Exception {
+        ExchangeCard cardToExchange = new ExchangeCard(
+                "1",
+                "Pikachu",
+                "BlaBla",
+                "search",
+                "12",
+                "Two Glurak"
+
+        );
+        exchangeRepo.save(cardToExchange);
+
+        mockMvc.perform(delete("/api/exchange/" + cardToExchange.id()))
+                .andExpect(status().isOk());
+    }
+
+
 }
