f727321bcc6ced9d0ed26fdfb6f1cb145c2b4429
Marcus <marcus.schweizer91@gmail.com>
2023-01-05 16:10:19 +0100

    Add SecurityConfig.java

---
+++ backend/src/main/java/com/example/backend/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity httpSecurity
+    ) throws Exception {
+        return httpSecurity
+                .csrf().disable()
+                .httpBasic()
+                .authenticationEntryPoint((request, response, authException) ->
+                response
+                        .sendError(
+                                HttpStatus.UNAUTHORIZED.value(),
+                                HttpStatus.UNAUTHORIZED.getReasonPhrase()))
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/cards").permitAll()
+                .antMatchers("/api/users/me").permitAll()
+                .antMatchers("/api/users/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .build();
+    }
+}
