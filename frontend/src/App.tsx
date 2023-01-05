706884373e4964dcc4922b6a5dbe225c8ee88b9d
Marcus <marcus.schweizer91@gmail.com>
2023-01-05 16:12:29 +0100

    Add username, login, logout, protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,20 +6,32 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import ExchangeApp from "./CardGallery/components/CardExchange/ExchangeApp";
 import PokemonDetails from "./CardGallery/components/PokemonCards/PokemonDetails";
 import ExchangeDetails from "./CardGallery/components/CardExchange/ExchangeDetails";
+import LoginPage from "./CardGallery/components/LoginPage";
+import useUser from "./CardGallery/components/hooks/useUser";
+import ProtectedRoutes from "./CardGallery/components/ProtectedRoutes";
 
 function App() {
+
+    const {username, login, logout} = useUser();
 
 
   return (
     <div className="App">
         <BrowserRouter>
-            <NavBar/>
+            <NavBar logout={logout}/>
+            <h2>{username}</h2>
             <Routes>
                 <Route path={"/"} element={<PokemonApp/>}></Route>
-                <Route path={"/exchange"} element={<ExchangeApp/>}></Route>
-                <Route path={"/user"} element={<PokemonApp/>}></Route>
-                <Route path={"/details/:id"} element={<PokemonDetails/>}></Route>
-                <Route path={"/exchange/:id"} element={<ExchangeDetails/>}></Route>
+                <Route path={"/login"} element={<LoginPage login={login}/>}></Route>
+
+                <Route element={
+                    <ProtectedRoutes username={username} />
+                }>
+                    <Route path={"/exchange"} element={<ExchangeApp/>}></Route>
+                    <Route path={"/details/:id"} element={<PokemonDetails/>}></Route>
+                    <Route path={"/exchange/:id"} element={<ExchangeDetails/>}></Route>
+                    <Route path={"/users"} element={<PokemonApp/>}></Route>
+                </Route>
             </Routes>
         </BrowserRouter>
     </div>
