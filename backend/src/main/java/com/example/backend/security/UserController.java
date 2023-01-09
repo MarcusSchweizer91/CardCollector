76ba3bc38330c6edb43f8783a2da1e29704265de
Marcus <marcus.schweizer91@gmail.com>
2023-01-09 12:50:21 +0100

    Add Post "/register"

--- backend/src/main/java/com/example/backend/security/UserController.java
+++ backend/src/main/java/com/example/backend/security/UserController.java
@@ -3,10 +3,7 @@ package com.example.backend.security;
 
 
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.security.Principal;
@@ -14,6 +11,12 @@ import java.security.Principal;
 @RestController
 @RequestMapping("api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/me")
     public String helloMe(Principal principal){
@@ -33,6 +36,11 @@ public class UserController {
         httpSession.invalidate();
         SecurityContextHolder.clearContext();
         return "unknownUser";
+    }
+
+    @PostMapping ("/register")
+    public MongoUser addUser (@RequestBody MongoUserDTO mongoUserDTO){
+        return userService.addUser(mongoUserDTO);
     }
 
 
