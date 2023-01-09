9c1b1e5b93cf0e08e5f747954f40dc2b7c6b6f01
Marcus <marcus.schweizer91@gmail.com>
2023-01-09 12:48:32 +0100

    Add favorite list & email to MongoUser

--- backend/src/main/java/com/example/backend/security/MongoUser.java
+++ backend/src/main/java/com/example/backend/security/MongoUser.java
@@ -3,11 +3,15 @@ package com.example.backend.security;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Document("users")
 public record MongoUser (
         @Id
         String id,
         String username,
-        String password
+        String password,
+        String email,
+        List<String> favorites
 ){
 }
