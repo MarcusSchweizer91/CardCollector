b9e57963cc2d0dd7f7f829b198a04d0936f365a8
Marcus <marcus.schweizer91@gmail.com>
2023-01-10 14:00:29 +0100

    Add unique fields in MongoDB with unique indexes

--- backend/src/main/java/com/example/backend/security/MongoUser.java
+++ backend/src/main/java/com/example/backend/security/MongoUser.java
@@ -1,6 +1,7 @@
 package com.example.backend.security;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -9,8 +10,10 @@ import java.util.List;
 public record MongoUser (
         @Id
         String id,
+        @Indexed(unique = true)
         String username,
         String password,
+        @Indexed(unique = true)
         String email,
         List<String> favorites
 ){
