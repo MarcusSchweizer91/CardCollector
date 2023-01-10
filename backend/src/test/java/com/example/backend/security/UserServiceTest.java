a95ae4a961e438fe41ba5e6242b48c3a827befaf
Marcus <marcus.schweizer91@gmail.com>
2023-01-10 13:27:53 +0100

    Add ServiceTest register

--- backend/src/test/java/com/example/backend/security/UserServiceTest.java
+++ backend/src/test/java/com/example/backend/security/UserServiceTest.java
@@ -6,11 +6,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class UserServiceTest {
 
@@ -52,5 +52,36 @@ class UserServiceTest {
         // THEN
 
         assertEquals(expected.username(),user.getUsername());
+    }
+
+    @Test
+    void registerUser(){
+        //Given
+        String id = "1234";
+        MongoUserDTO mongoUserDTO = new MongoUserDTO(
+                "Hans",
+                "123",
+                "test",
+                List.of("a","b"));
+
+        // When
+        when(idService.generateID()).thenReturn(id);
+        when(argon2EncoderService.encode("123")).thenReturn("encoded_password");
+
+        MongoUser expectedUser = new MongoUser(
+                id,
+                "Hans",
+                "encoded_password",
+                "test",
+                List.of("a","b"));
+
+        when(mongoUserRepo.save(expectedUser)).thenReturn(expectedUser);
+        MongoUser returnedUser = userService.addUser(mongoUserDTO);
+
+                //Then
+        assertEquals(expectedUser, returnedUser);
+        verify(idService, times(1)).generateID();
+        verify(argon2EncoderService, times(1)).encode("123");
+        verify(mongoUserRepo, times(1)).save(expectedUser);
     }
 }
