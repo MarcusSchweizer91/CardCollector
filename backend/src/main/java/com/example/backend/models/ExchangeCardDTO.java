2f0d06aea7a4b2bf8fa9ec81efe02a6591fe3268
Marcus <marcus.schweizer91@gmail.com>
2023-01-16 14:59:47 +0100

    Add Attribute

--- backend/src/main/java/com/example/backend/models/ExchangeCardDTO.java
+++ backend/src/main/java/com/example/backend/models/ExchangeCardDTO.java
@@ -1,11 +1,16 @@
 package com.example.backend.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.web.multipart.MultipartFile;
+
 public record ExchangeCardDTO(
 
         String name,
         String description,
         String type,
         String price,
-        String alternative
+        String alternative,
+       @JsonIgnore
+        MultipartFile cardImage
 ) {
 }
