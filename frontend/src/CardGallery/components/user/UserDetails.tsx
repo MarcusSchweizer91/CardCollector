0c2061da7e104d6969aaaf78d71ae5be6d704b5d
Marcus <marcus.schweizer91@gmail.com>
2023-01-22 02:11:55 +0100

    Add const favCards

--- frontend/src/CardGallery/components/user/UserDetails.tsx
+++ frontend/src/CardGallery/components/user/UserDetails.tsx
@@ -1,15 +1,19 @@
 
 import {UserData} from "../../models/UserData";
+import FavCard from "./FavCard";
 
 
 type UserDetailsProps={
     userInfo?: UserData
 
+
 }
 
 export default function UserDetails(props: UserDetailsProps){
 
-
+    const favCards = props.userInfo?.favorites.map(favData =>{
+        return <FavCard favorite={favData} key={favData.id}/>
+    })
 
 
     return(
@@ -22,7 +26,7 @@ export default function UserDetails(props: UserDetailsProps){
                     <h2>{props.userInfo.username}</h2>
                     <p>{props.userInfo.email}</p>
                     <p>{props.userInfo.password}</p>
-                    <p>{props.userInfo.favorites}</p>
+                    <p>{favCards}</p>
                 </div>
 
 
