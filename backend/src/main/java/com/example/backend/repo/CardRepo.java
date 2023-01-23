6c7e3f71fb915629f937abab5dbd1d5fe509b0c5
Marcus <marcus.schweizer91@gmail.com>
2023-01-23 12:02:18 +0100

    Rename Card

--- backend/src/main/java/com/example/backend/repo/CardRepo.java
+++ backend/src/main/java/com/example/backend/repo/CardRepo.java
@@ -1,9 +1,9 @@
 package com.example.backend.repo;
 
-import com.example.backend.models.Card;
+import com.example.backend.models.PokeCard;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CardRepo extends MongoRepository <Card, String>{
+public interface CardRepo extends MongoRepository <PokeCard, String>{
 }
