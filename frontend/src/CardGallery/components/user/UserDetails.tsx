05349ed2936f4de285e6e133c34e54121c07ab9f
Marcus <marcus.schweizer91@gmail.com>
2023-01-24 00:17:55 +0100

    Add favorite Cards

--- frontend/src/CardGallery/components/user/UserDetails.tsx
+++ frontend/src/CardGallery/components/user/UserDetails.tsx
@@ -1,36 +1,104 @@
-
 import {UserData} from "../../models/UserData";
-import FavCard from "./FavCard";
+import {useEffect, useState} from "react";
+import useCards from "../hooks/useCards";
+import {PokeCard} from "../../models/PokeCard";
+import {
+    Button,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Grid,
+    Typography
+} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
+type UserDetailsProps = {
 
-type UserDetailsProps={
     userInfo?: UserData
-
-
+    getFavoriteCards: () => Promise<string[]>
 }
 
-export default function UserDetails(props: UserDetailsProps){
+export default function UserDetails(props: UserDetailsProps) {
+    const {userInfo, getFavoriteCards} = props;
+    const {pokeCards} = useCards();
+    const [cardsToDisplay, setCardsToDisplay] = useState<PokeCard[]>([]);
 
-    const favCards = props.userInfo?.favorites.map(favData =>{
-        return <FavCard favorite={favData} key={favData.id}/>
-    })
+
+    useEffect(() => {
+        getFavoriteCards().then(favorites => {
+            // @ts-ignore
+            const cardsToDisplay = pokeCards.filter(card => favorites.map(f => f.id).includes(card.id));
+            setCardsToDisplay(cardsToDisplay);
+        });
+    }, [pokeCards, getFavoriteCards]);
 
 
-    return(
+    const navigate = useNavigate()
+
+    function handleDetailsClick(cardId: string) {
+        navigate(`/details/${cardId}`);
+    }
+
+
+
+
+    return (
         <div>
-            {! props.userInfo && <p>loading...</p>}
-            {props.userInfo &&
-
+            {!userInfo && <p>loading...</p>}
+            {userInfo &&
                 <div>
+                    <Card>
+                        <h2>UserData:</h2>
+                        <h3>{userInfo.username}</h3>
+                        <p>{userInfo.email}</p>
 
-                    <h2>{props.userInfo.username}</h2>
-                    <p>{props.userInfo.email}</p>
-                    <p>{props.userInfo.password}</p>
-                    <p>{favCards}</p>
+                    </Card>
+
+                    <h2>Favorite Cards:</h2>
+                    <Grid container justifyContent={"space-evenly"}>
+                        {cardsToDisplay.map(card => (
+
+
+                            <Card key={card.id}>
+                                <CardActionArea onClick={()=>handleDetailsClick(card.id)}>
+                                    <div key={card.id}>
+                                        <CardMedia
+                                            component="img"
+                                            image={card.images.small}
+                                            alt="Pokeball"
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="div">
+                                                {card.name} <br/>
+                                                {card.hp}
+                                            </Typography>
+                                        </CardContent>
+                                    </div>
+                                </CardActionArea>
+                                <CardActions>
+
+                                    <Button href="/exchange" variant={"outlined"} size="small" color="primary">
+                                        Buy
+                                    </Button>
+                                    <Button href="/exchange" variant={"outlined"} size="small" color="primary">
+                                        Exchange
+                                    </Button>
+                                    <Button variant={"outlined"} size="small" color="primary">
+                                        Delete
+                                    </Button>
+                                </CardActions>
+                            </Card>
+
+
+                        ))}
+                    </Grid>
+
                 </div>
-
-
             }
         </div>
-    )
+    );
+
+
 }
