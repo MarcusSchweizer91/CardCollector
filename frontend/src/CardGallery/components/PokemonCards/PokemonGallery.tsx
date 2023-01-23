3120b2b875bbbc97feb715dc54b0f6871edef982
Marcus <marcus.schweizer91@gmail.com>
2023-01-24 00:22:24 +0100

    Add props

--- frontend/src/CardGallery/components/PokemonCards/PokemonGallery.tsx
+++ frontend/src/CardGallery/components/PokemonCards/PokemonGallery.tsx
@@ -4,13 +4,21 @@ import { Container, Grid} from "@mui/material";
 
 type PokemonGalleryProps = {
     cards: PokeCard[]
+
     addCardToFavorites(id:string):void
+    isCardInFavorites:(cardId: string) => boolean
+    removeCardFromFavorites: (cardId: string) => Promise<void>;
 }
 
 export default function PokemonGallery(props: PokemonGalleryProps) {
 
     const cardComponents = props.cards.map(cardData => {
-        return <PokemonCard addCardToFavorites={props.addCardToFavorites} card={cardData} key={cardData.id}/>
+        return <PokemonCard
+                            removeCardFromFavorites={props.removeCardFromFavorites}
+                            isCardInFavorites={props.isCardInFavorites}
+                            addCardToFavorites={props.addCardToFavorites}
+                            card={cardData}
+                            key={cardData.id}/>
     })
 
     return (
