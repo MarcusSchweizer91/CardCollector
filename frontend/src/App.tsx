b705f37cf847bc8fb8e7d28bd8a54f12f595bc4b
Marcus <marcus.schweizer91@gmail.com>
2023-01-24 00:23:59 +0100

    update useFavorites

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,22 +11,23 @@ import useUser from "./CardGallery/components/hooks/useUser";
 import ProtectedRoutes from "./CardGallery/components/ProtectedRoutes";
 import RegisterForm from "./CardGallery/components/user/RegisterForm";
 import UserDetails from "./CardGallery/components/user/UserDetails";
-import useFavorites from "./CardGallery/components/hooks/UseFavorites";
+import useFavorites from "./CardGallery/components/hooks/useFavorites";
+
+
 
 
 function App() {
 
     const {userInfo, login, logout, register} = useUser();
 
-    const{favorites, getFavoriteCards} = useFavorites()
+    const{favorites, addCardToFavorites, getFavoriteCards, isCardInFavorites, removeCardFromFavorites} = useFavorites()
 
   return (
     <div className="App">
         <BrowserRouter>
             <NavBar logout={logout}/>
-            <h2>{userInfo?.username}</h2>
             <Routes>
-                <Route path={"/"} element={<PokemonApp/>}></Route>
+                <Route path={"/"} element={<PokemonApp addCardToFavorites={(id) => addCardToFavorites(id, favorites)} removeCardFromFavorites={removeCardFromFavorites} isCardInFavorites={isCardInFavorites}/>}></Route>
                 <Route path={"/login"} element={<LoginPage login={login}/>}></Route>
                 <Route path={"/register"} element={<RegisterForm register={register}/>}></Route>
 
@@ -36,7 +37,7 @@ function App() {
                     <Route path={"/exchange"} element={<ExchangeApp/>}></Route>
                     <Route path={"/details/:id"} element={<PokemonDetails/>}></Route>
                     <Route path={"/exchange/:id"} element={<ExchangeDetails/>}></Route>
-                    <Route path={"/user"} element={<UserDetails userInfo={userInfo}/>}></Route>
+                    <Route path={"/user"} element={<UserDetails getFavoriteCards={getFavoriteCards}  userInfo={userInfo}/>}></Route>
                 </Route>
             </Routes>
         </BrowserRouter>
