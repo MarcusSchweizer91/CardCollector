50673004230774757b96f45d6ac58e4b2bcaaa1a
Marcus <marcus.schweizer91@gmail.com>
2023-01-26 15:34:02 +0100

    Add interface CostCounter

--- frontend/src/CardGallery/models/Attacks.ts
+++ frontend/src/CardGallery/models/Attacks.ts
@@ -1,7 +1,12 @@
+export interface CostCounter {
+    [key: string]: number;
+}
+
 
 export type Attacks ={
 
     name:string,
     cost: string[],
+    costCounts: CostCounter,
     damage:string
 }
