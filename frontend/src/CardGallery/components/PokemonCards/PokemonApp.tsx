d84b4ea70a84b1e268bb7f346b9bdffa4db64da9
Marcus <marcus.schweizer91@gmail.com>
2023-01-27 13:01:22 +0100

    Add header background

--- frontend/src/CardGallery/components/PokemonCards/PokemonApp.tsx
+++ frontend/src/CardGallery/components/PokemonCards/PokemonApp.tsx
@@ -2,12 +2,14 @@ import {useState} from "react";
 import PokemonGallery from "./PokemonGallery";
 import SearchBar from "../SearchBar";
 import useCards from "../hooks/useCards";
+import "../css/PokemonApp.css"
+import bg from "../../img/pokemon-pokemon-red-and-blue-blastoise-pokemon-bulbasaur-pokemon-wallpaper-preview.jpeg";
 
 
 type PokemonAppProps = {
 
-    isCardInFavorites:(cardId: string) => boolean
-    addCardToFavorites(id:string):void
+    isCardInFavorites: (cardId: string) => boolean
+    addCardToFavorites(id: string): void
     removeCardFromFavorites: (cardId: string) => Promise<void>
 }
 export default function PokemonApp(props: PokemonAppProps) {
@@ -29,14 +31,25 @@ export default function PokemonApp(props: PokemonAppProps) {
     return (
         <div>
             <div>
-                <SearchBar handleSearchText={handleSearchOnChange}/>
-                <PokemonGallery
-                                removeCardFromFavorites={props.removeCardFromFavorites}
-                                isCardInFavorites={props.isCardInFavorites}
-                                addCardToFavorites={props.addCardToFavorites}
-                                cards={filteredPokeCards}/>
-            </div>
+                <div className={"header-img"}>
+                    <div className={"header"}>
+                        <div className={"header-text"}>
+                            <div className="  ">
+                                <h1 className="card-title title">Welcome.</h1>
+                                <h3 className="card-text pb-2 title">Discover the world of trading cards...</h3>
+                            </div>
+                        </div>
+                        <img src={bg} className="card-img" alt="Background"/>
+                        <SearchBar handleSearchText={handleSearchOnChange}/>
+                    </div>
+                    <PokemonGallery
+                        removeCardFromFavorites={props.removeCardFromFavorites}
+                        isCardInFavorites={props.isCardInFavorites}
+                        addCardToFavorites={props.addCardToFavorites}
+                        cards={filteredPokeCards}/>
+                </div>
 
+            </div>
         </div>
     )
 }
