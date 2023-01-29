d8e3695381d1d9e259c5e5a5dd3d392a2d5c19c1
Marcus <marcus.schweizer91@gmail.com>
2023-01-29 04:32:13 +0100

    Update return logout

--- frontend/src/CardGallery/components/hooks/useUser.ts
+++ frontend/src/CardGallery/components/hooks/useUser.ts
@@ -18,7 +18,7 @@ export default function useUser() {
             .then(response => response.data)
             .then((data)=>{
                 setUsername(data)
-                return data
+                return "Successfully logged out!"
             })
     }
 
