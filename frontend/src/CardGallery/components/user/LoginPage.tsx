33fd875b094cf5e7ba4ce6957dde4d285f951375
Marcus <marcus.schweizer91@gmail.com>
2023-01-29 04:51:36 +0100

    Update snackbar already loggedIn & loginSucess

--- frontend/src/CardGallery/components/user/LoginPage.tsx
+++ frontend/src/CardGallery/components/user/LoginPage.tsx
@@ -1,22 +1,57 @@
 import {Button, TextField} from "@mui/material";
 import Box from "@mui/material/Box";
-import React, {ChangeEvent, FormEvent, useState} from "react";
+import React, {ChangeEvent, FormEvent, useEffect, useState} from "react";
 import "../css/LoginPage.css";
 
 import {useNavigate} from "react-router-dom";
 import SendIcon from "@mui/icons-material/Send";
 import useAuthCheck from "../hooks/useAuthCheck";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, {AlertProps} from "@mui/material/Alert";
+
 
 type LoginPageProps={
     login: (username:string, password:string)=> Promise<string>
 }
 
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref,
+) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 export default function LoginPage(props: LoginPageProps){
 
     const [username, setUsername] = useState<string>("")
     const [password, setPassword] = useState<string>("")
-
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+    const [snackbarMessageError, setSnackbarMessageError] = useState("");
+    const [snackbarMessageSuccess, setSnackbarMessageSuccess] = useState("");
     const navigate = useNavigate()
+    const isLoggedIn = useAuthCheck();
+
+
+    useEffect(() => {
+        if (isLoggedIn) {
+            setOpenSnackbar(true);
+            setSnackbarMessageError("Already logged in!");
+            setTimeout(() => {
+                navigate("/");
+            }, 3000); //3000ms bis zur Umleitung
+        }
+    }, [isLoggedIn, navigate])
+
+
+
+    const handleCloseSnackbar = (event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpenSnackbar(false);
+        setSnackbarMessageSuccess("");
+        setSnackbarMessageError("");
+    }
 
 
 
@@ -28,19 +63,25 @@ export default function LoginPage(props: LoginPageProps){
         setPassword(event.target.value)
     }
 
-    function loginSubmit(event: FormEvent<HTMLFormElement>){
+    function loginSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
 
         props.login(username,password)
-            .then(user => {
-                navigate("/")
+            .then(() => {
+                setOpenSnackbar(true);
+                setSnackbarMessageSuccess("Successfully logged in!");
+                setTimeout(() => {
+                    navigate("/");
+                }, 1500);
             })
+            .catch(() => {
+                setOpenSnackbar(true);
+                setSnackbarMessageError("Invalid username or password!");
+            });
     }
 
-    const isLoggedIn = useAuthCheck();
-    if (isLoggedIn) {
-        navigate("/");
-    }
+
+
     return(
 
         <div>
@@ -73,6 +114,12 @@ export default function LoginPage(props: LoginPageProps){
                     Login
                 </Button>
             </Box>
+            <Snackbar open={openSnackbar} autoHideDuration={3000} onClose={handleCloseSnackbar}>
+                <Alert onClose={handleCloseSnackbar} severity={snackbarMessageError ? "error" : "success"} sx={{ width: '100%' }}>
+                    {snackbarMessageError || snackbarMessageSuccess}
+                </Alert>
+            </Snackbar>
+
         </div>
     )
 }
