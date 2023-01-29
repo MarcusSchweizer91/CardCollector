8d1441682ec05884bad67ed332d206f341b902e1
Marcus <marcus.schweizer91@gmail.com>
2023-01-29 18:07:05 +0100

    Add style ExchangeDetails

--- frontend/src/CardGallery/components/CardExchange/ExchangeDetails.tsx
+++ frontend/src/CardGallery/components/CardExchange/ExchangeDetails.tsx
@@ -3,6 +3,8 @@ import {useEffect, useState} from "react";
 
 import axios from "axios";
 import {CardToExchange} from "../../models/CardToExchange";
+import {Paper, Stack} from "@mui/material";
+import Box from "@mui/material/Box";
 
 
 export default function ExchangeDetails() {
@@ -34,13 +36,21 @@ export default function ExchangeDetails() {
             {!exchangeEntry && <p>loading...</p>}
             {exchangeEntry &&
 
-                <div>
-                    <h2>{exchangeEntry.name}</h2>
-                    <p>{exchangeEntry.description}</p>
-                    <p>{exchangeEntry.type}</p>
-                    <p>{exchangeEntry.price}</p>
-                    <p>{exchangeEntry.alternative}</p>
-                </div>
+                <Stack spacing={1}>
+                    <Paper>
+                        <Paper >
+                            <h2>{exchangeEntry.name}</h2>
+                        </Paper>
+                        <Box>
+                            <p>Card description: {exchangeEntry.description}</p>
+                        </Box>
+                        <div>
+                            <p>Search or Offer: {exchangeEntry.type}</p>
+                            <p>Price: {exchangeEntry.price} EUR</p>
+                            <p>Alternative Trade: {exchangeEntry.alternative}</p>
+                        </div>
+                    </Paper>
+                </Stack>
 
 
             }
